a989ddae71eda2d85c2349543065ae80694d5bfa
Justin Phillips <justinphil02@yahoo.com>
2019-10-31 23:24:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+
 BRANDS = []
 
   def initialize(brand)
@@ -17,11 +17,7 @@ BRANDS = []
     puts "Your shoe is as good as new!"
   end
 
-def brand=(brand)
-@brand = brand
-  if BRANDS.include? == false
-  BRANDS << brand
-  end
+
 end
 
 end
